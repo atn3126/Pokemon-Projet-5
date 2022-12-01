f33aef8bd3ac94d4b1af9e4064fdb24f00e00492
Riseik <emaillard@gaming.tech>
2022-12-01 08:28:46 +0100

    map + walls (don't work)

---
+++ Map/assets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="assets" tilewidth="16" tileheight="16" tilecount="21000" columns="140">
+ <image source="../Projet 5/assets.png" width="2240" height="2400"/>
+</tileset>
--- const.h
+++ const.h
@@ -4,7 +4,7 @@
 const int WINDOW_SIZE_X = 900;
 const int WINDOW_SIZE_Y = 900;
 const float SPEED = 30;
-const int SIZE_TILE = 30;
+const int SIZE_TILE = 16;
 const int ANIM = 2;
 
 
